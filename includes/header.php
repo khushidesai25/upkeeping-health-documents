3f918c4ddebb6389367799d65e6cc983081be6f6
khushidesai25 <162878652+khushidesai25@users.noreply.github.com>
2025-08-02 08:27:55 +0530

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Health Document System</title>
+    <link rel="stylesheet" type="text/css" href="assests/css/style.css">
+</head>
+<body>
+<header>
+    <h1>Health Document System</h1>
+</header>
